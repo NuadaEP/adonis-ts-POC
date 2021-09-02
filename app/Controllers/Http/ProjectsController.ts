a8cfd0018e96c61c32fd9f500541687e9b7cc058
Bruno Cardoso <bruno@liveonsolutions.com>
2021-09-01 21:50:34 -0300

    add validator

--- app/Controllers/Http/ProjectsController.ts
+++ app/Controllers/Http/ProjectsController.ts
@@ -1,9 +1,17 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+
 import Project from 'App/Models/Project'
 
 export default class ProjectsController {
   public async create({ request }: HttpContextContract): Promise<Project> {
     const { projectName } = request.only(['projectName'])
+
+    const schemaValidator = schema.create({
+      projectName: schema.string(),
+    })
+
+    await request.validate({ schema: schemaValidator })
 
     const project = await Project.create({
       name: projectName,
